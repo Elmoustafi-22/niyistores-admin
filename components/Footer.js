d630582876619af04654167624e973f7892087cf
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2024-09-20 17:31:45 +0100

    Added Footer component

---
+++ components/Footer.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return <>
+    <footer className="bg-gray-50 border-t border-zinc-200">
+        <div className="mx-auto max-w-screen-2xl px-4 sm:px-6 lg:px-8">
+            <div className="sm:flex sm:items-center sm:justify-between">
+                <div className="flex justify-center text-teal-600 sm:justify-start">
+                    <span className="sr-only">Home</span>
+                    <Link 
+                      href="/"
+                      className=""
+                    >
+                      <svg
+                        className="h-8 w-8 text-teal-600"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                        />
+                      </svg>
+                      Admin
+                    </Link>
+                </div>
+                <p className="mt-4 text-center text-sm text-gray-900 lg:mt-0 lg:text-right">
+                    Copyright &copy; {new Date().getFullYear()}. All rights reserved
+                </p>
+            </div>
+        </div>
+    </footer>
+  </>
+}
--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,22 @@ import React from "react";
 import { Montserrat } from "next/font/google";
 import { SessionProvider } from "next-auth/react"
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast"
 
 const inter = Montserrat({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <main
-      className={`mx-auto max-w-screen-7xl px-4 sm:px-6 lg:px-8 ${inter.className}`}
-    >
-      <SessionProvider session={session}>
+    <SessionProvider session={session}>
+      <main
+        className={`mx-auto max-w-screen-7xl px-4 sm:px-6 lg:px-8 ${inter.className}`}
+      >
         <Header />
         <Component {...pageProps} />
-        <Toaster 
-          position="top-center"
-          reverseOrder={false}
-        />
-      </SessionProvider>
-    </main>
+        <Toaster position="top-center" reverseOrder={false} />
+        <Footer />
+      </main>
+    </SessionProvider>
   );
 }
